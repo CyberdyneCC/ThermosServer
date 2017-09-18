f043d1a3036c4f8fa6bcd59bb6262bd3e327b8bd
Joseph Robinson <smartech56@gmail.com>
2017-09-18 14:43:42 -0700

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,6 +1,6 @@
 echo "Using advanced Linux startup"
 # linux startup commands here
-sudo apt update && sudo apt update
+sudo apt update && sudo apt upgrade
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt update
 sudo apt-get -y install oracle-java8-installer
