1e8fd12a2232e1c673ac4d2232f097c287a05127
Joseph Robinson <smartech56@gmail.com>
2017-09-18 14:39:50 -0700

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,1 +1,7 @@
+echo "Using advanced Linux startup"
+echo "Executing start.bat"
+echo "Checking environment..."
+java -version
+echo "starting Thermos..."
 java -XX:+UseG1GC -XX:StringTableSize=1000003 -XX:+UseFastAccessorMethods -XX:+OptimizeStringConcat -XX:MetaspaceSize=512m -XX:MaxMetaspaceSize=4096m -XX:+AggressiveOpts -XX:MaxGCPauseMillis=50 -XX:+UseStringDeduplication -Xms4G -Xmx6G -XX:hashCode=5 -Dfile.encoding=UTF-8 -jar Thermos.jar --log-strip-color
+echo "Remember to name thermos jar file Thermos"
