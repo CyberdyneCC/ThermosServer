0047116027bf4708451e2aba2caaa90cdef494ea
Joseph Robinson <smartech56@gmail.com>
2017-09-18 14:43:26 -0700

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,5 +1,9 @@
 echo "Using advanced Linux startup"
 # linux startup commands here
+sudo apt update && sudo apt update
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt update
+sudo apt-get -y install oracle-java8-installer
 echo "Executing start.bat"
 # pasted from start.bat
 # LOL
