6e1289468f6bd21d92cf961ddf33dca39e387d7b
Joseph Robinson <smartech56@gmail.com>
2018-01-01 09:34:55 -0700

    Update launch.bat

--- launch.bat
+++ launch.bat
@@ -1,5 +1,5 @@
 echo "Checking environment..."
 java -version
 echo "starting Thermos..."
-java -XX:+UseG1GC -XX:StringTableSize=1000003 -XX:+UseFastAccessorMethods -XX:+OptimizeStringConcat -XX:MetaspaceSize=512m -XX:MaxMetaspaceSize=4096m -XX:+AggressiveOpts -XX:MaxGCPauseMillis=50 -XX:+UseStringDeduplication -Xms4G -Xmx6G -XX:hashCode=5 -Dfile.encoding=UTF-8 -jar Thermos.jar --log-strip-color
+java -XX:+UseG1GC -XX:StringTableSize=1000003 -XX:+UseFastAccessorMethods -XX:+OptimizeStringConcat -XX:MetaspaceSize=512m -XX:MaxMetaspaceSize=4096m -XX:+AggressiveOpts -XX:MaxGCPauseMillis=50 -XX:+UseStringDeduplication -Xms512M -Xmx6G -XX:hashCode=5 -Dfile.encoding=UTF-8 -jar Thermos.jar --log-strip-color
 echo "Remember to name thermos jar file Thermos"
