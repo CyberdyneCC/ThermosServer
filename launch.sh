8bee472c6338a6d4f930333e7c19dfaafd382ee8
Joseph Robinson <smartech56@gmail.com>
2018-01-01 09:37:33 -0700

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,9 +1,6 @@
 echo "Using advanced Linux startup"
 # linux startup commands here
-sudo apt update && sudo apt upgrade
-sudo add-apt-repository ppa:webupd8team/java
-sudo apt update
-sudo apt-get -y install oracle-java8-installer
+sudo apt -y install git default-jre default-jdk
 echo "Executing start.bat"
 # pasted from start.bat
 # LOL
@@ -12,4 +9,3 @@ java -version
 # Environment check /\
 echo "starting Thermos..."
 java -XX:+UseG1GC -XX:StringTableSize=1000003 -XX:+UseFastAccessorMethods -XX:+OptimizeStringConcat -XX:MetaspaceSize=512m -XX:MaxMetaspaceSize=4096m -XX:+AggressiveOpts -XX:MaxGCPauseMillis=50 -XX:+UseStringDeduplication -Xms4G -Xmx6G -XX:hashCode=5 -Dfile.encoding=UTF-8 -jar Thermos.jar --log-strip-color
-echo "Remember to name thermos jar file Thermos"
